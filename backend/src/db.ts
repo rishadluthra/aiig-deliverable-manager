3400713a16455760e154dfa521385ccd45ed4724
Rishad <rishadluthra5@gmail.com>
2026-02-02 21:10:09 -0500

    cleanup

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,12 @@
+// Instantiating Prisma Client with PostgreSQL adapter
+// https://www.prisma.io/docs/getting-started/prisma-orm/add-to-existing-project/postgresql#7-instantiate-prisma-client
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
+// Connects Prisma Client to PostgreSQL using the db url from .env
 const connectionString = process.env.DATABASE_URL!
-
+// allows us to use PostgreSQL with Prisma instead of just SQLite
 const adapter = new PrismaPg({ connectionString })
+// export one client instance to be used across the app
 export const prisma = new PrismaClient({ adapter })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+// middleware for setting various HTTP headers for app security - prevents injection attacks etc
 import helmet from "helmet";
 import dotenv from "dotenv";
 
@@ -18,17 +19,18 @@ app.use(express.json());
 // Call: GET /api/projects?search=keyword
 // Returns: [{id, name}]
 app.get("/api/projects", async (req, res) => {
+  // extract the optional search query parameter
   const { search } = req.query as { search?: string };
   try {
     const projects = await prisma.project.findMany({
       where: search
-        ? {
+        ? { // if search parameter is provide we filter projects by name
             name: {
-              contains: search,
+              contains: search, // enable partial matching, filter projects whose name contains the search keyword
               mode: "insensitive",
             },
           }
-        : {},
+        : {}, // else we don't filter and just return all projects
       orderBy: { name: "asc" },
     });
     res.json(projects);
@@ -42,12 +44,16 @@ app.get("/api/projects", async (req, res) => {
 // Returns: [{id, title, dueDate, project}]
 app.get("/api/projects/:id/deliverables", async (req, res) => {
   try {
+    // parse projectId from params and validate it
     const projectId = parseInt(req.params.id);
     if (isNaN(projectId))
       return res.status(400).json({ error: "Invalid project ID" });
 
     const deliverables = await prisma.deliverable.findMany({
+      // filter deliverables by projectId
       where: { projectId },
+      // syntax for Prisma to do a SQL JOIN to include related project data
+      // https://www.prisma.io/docs/orm/prisma-client/queries/relation-queries#examples
       include: { project: true },
       orderBy: { dueDate: "asc" },
     });
